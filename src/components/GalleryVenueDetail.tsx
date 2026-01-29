567e842fdea08258fd839ca957fe2c147a6b4102
Mark Barnes <markbarnes@Marks-MacBook-Pro-M4.local>
2026-01-29 10:24:01 +0000

    Fix pinned images for killeavy-castle venue

--- src/components/GalleryVenueDetail.tsx
+++ src/components/GalleryVenueDetail.tsx
@@ -60,10 +60,10 @@ const PINNED: Record<string, string[]> = {
 
 
   "killeavy-castle": [
+    "MKB_weddings_Ireland_Northen_ireland_Wedding_Photography_killeavy-castle_Wedding_Photography-462_500.webp",
     "mkb-weddings-northern-ireland-wedding-photographer-killeavy-castle-newry-wedding-photography-116_500.webp",
     "MKB-weddings-mkb-photography-northern-ireland-wedding-photographer-killeavy-castle-newry-wedding-photography6_500.webp",
     "MKB_weddings_mkb_Photography-Northern-ireland-wedding-photography-northern-ireland-wedding-photographer-killeavy-castle-wedding-photography-100_500.webp",
-    "MKB_weddings_Ireland_Northen_ireland_Wedding_Photography_killeavy-castle_Wedding_Photography-462_500.webp",
     "MKB_weddings_Ireland_Northen_ireland_Wedding_Photography_killeavy-castle_Wedding_Photography-609_500.webp",
 
 ],
