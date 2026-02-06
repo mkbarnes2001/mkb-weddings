ad5256868c0318a942c2c5e3f7fc861912a61617
Mark Barnes <markbarnes@Marks-MacBook-Pro-M4.local>
2026-02-06 21:38:43 +0000

    src/components/GalleryVenueDetail.tsx

--- src/components/GalleryVenueDetail.tsx
+++ src/components/GalleryVenueDetail.tsx
@@ -510,7 +510,7 @@ export function GalleryVenueDetail() {
       </section>
 
       {/* GRID (extra gap from description) */}
-      <div className="max-w-7xl mx-auto px-6 pb-24">
+      <div className="max-w-7xl mx-auto px-6 pb-40">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {images.map((img, idx) => (
             <button
