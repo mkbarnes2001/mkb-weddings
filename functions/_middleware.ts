8df57542d10840fdf73c7d2a45a80d4d38936826
Mark Barnes <markbarnes@mac.home>
2026-02-12 17:06:53 +0000

    fix routes

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,17 @@
 export async function onRequest(context: any) {
   const url = new URL(context.request.url);
 
+  // Don't touch static assets
+  if (
+    url.pathname.startsWith("/assets/") ||
+    url.pathname === "/robots.txt" ||
+    url.pathname === "/sitemap.xml" ||
+    url.pathname === "/favicon.ico" ||
+    url.pathname === "/site.webmanifest"
+  ) {
+    return context.next();
+  }
+
   // Match old WordPress-style URLs: /1234/anything
   const isOldWpPost = /^\/\d{4,}\/.+/.test(url.pathname);
 
